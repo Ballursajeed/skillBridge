3da14378af45b22f1b0ab80a656af638fff013e6
Ballursajeed <ballursajeed@gmail.com>
2026-05-07 04:01:40 +0530

    updated deployed live client url to cors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend.vercel.app"
+    "https://skill-bridge-omega-vert.vercel.app/"
   ],
   credentials: true
 }));
